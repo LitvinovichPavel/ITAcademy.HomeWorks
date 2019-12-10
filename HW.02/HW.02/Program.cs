cd1f37838dca072de30d23bbf661a2275a31393c
Pavel Litvinovich <21asdf@mail.ru>
2019-12-10 14:43:03 +0300

    HW.02

---
+++ HW.02/HW.02/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace HW._02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            StreamReader textReader = new StreamReader(@"C:\D\it\image.txt", true);
+            string textReaderResult = textReader.ReadToEnd();
+            string[] arrayOfTextResult = textReaderResult.Split(' ');
+            byte[] imageBytes = new byte[arrayOfTextResult.Length - 1];
+
+            for (int i = 0; i < arrayOfTextResult.Length - 1; i++)
+            {
+                byte binary = Convert.ToByte(arrayOfTextResult[i], 2);
+                imageBytes[i] = binary;
+            }
+
+            File.WriteAllBytes(@"C:\D\it\image.png", imageBytes);
+            textReader.Dispose();
+        }
+    }
+}
