38532a8b97cd522827909d267b0a00439c5407bd
Pavel Litvinovich <21asdf@mail.ru>
2019-12-10 14:54:03 +0300

    HW03.Task2

---
+++ HW03.Task2/HW03.Task2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace HW03.Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            sbyte sb1 = 11;
+            SByte sb2 = 11;
+            short sh1 = 22;
+            Int16 sh2 = 22;
+            int in1 = 33;
+            Int32 in2 = 33;
+            long lo1 = 44;
+            Int64 lo2 = 44;
+            byte by1 = 55;
+            Byte by2 = 55;
+            ushort us1 = 66;
+            UInt16 us2 = 66;
+            char ch1 = 'L';
+            Char ch2 = 'L';
+            uint ui1 = 88;
+            UInt32 ui2 = 88;
+            ulong ul1 = 99;
+            UInt64 ul2 = 99;
+            float fl1 = 100F;
+            Single fl2 = 100F;
+            double do1 = 101;
+            Double do2 = 101;
+            decimal de1 = 102;
+            Decimal de2 = 102;
+        }
+    }
+}
