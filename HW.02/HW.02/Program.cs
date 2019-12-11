2b584f6bfcc657ff6a2230998e77f2b01c4076d6
Pavel Litvinovich <21asdf@mail.ru>
2019-12-11 18:13:25 +0300

    final

--- HW.02/HW.02/Program.cs
+++ HW.02/HW.02/Program.cs
@@ -7,8 +7,17 @@ namespace HW._02
     {
         static void Main(string[] args)
         {
-
-            StreamReader textReader = new StreamReader(@"C:\D\it\image.txt", true);
+            /*1.указываем файл из которого будет происходить считывание кода.
+              2.считывается последовательный наборы символов
+              3.создаем строчный массив
+              4.создаем массив типа byte
+              5.запускаем цикл for который перебирая поочередно нолики и единички из нашего текстового документа, конвертирует из в пиксели
+              6.указывам место где будет сохранен сконвертированный файл и указываем его формат
+              7.запускаем сборщик мусора для освобождения памяти
+              8.пойду попантуюсь перед женой, что я крутой программист
+            */
+            
+           StreamReader textReader = new StreamReader(@"C:\D\it\image.txt", true);
             string textReaderResult = textReader.ReadToEnd();
             string[] arrayOfTextResult = textReaderResult.Split(' ');
             byte[] imageBytes = new byte[arrayOfTextResult.Length - 1];
