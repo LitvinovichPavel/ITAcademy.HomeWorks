a7dc338fef7b19488078cd1db34867cde5464d84
Pavel Litvinovich <21asdf@mail.ru>
2019-12-12 14:20:40 +0300

    HW04.Player

---
+++ HW04.2_Player/HW04.2_Player/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+
+
+namespace HW04._2_Player
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Задайте направление движения игрока с помощью клавиш W,S,A,D\nесли действий не требуется нажмите Q : ");
+            char player;
+            player = (char)Console.Read();
+            
+            switch (player)
+            {
+                case 'Q':
+                case 'q':
+                case 'Й':
+                case 'й':
+                    Console.WriteLine("Перемещение не требуется");
+                    break;
+                case 'W':
+                case 'w':
+                case 'Ц':
+                case 'ц':
+                    Console.WriteLine("Переместить вверх");
+                    break;
+                case 'S':
+                case 's':
+                case 'Ы':
+                case 'ы':
+                    Console.WriteLine("Переместить вниз");
+                    break;
+                case 'A':
+                case 'a':
+                case 'Ф':
+                case 'ф':
+                    Console.WriteLine("Переместить влево");
+                    break;
+                case 'D':
+                case 'd':
+                case 'В':
+                case 'в':
+                    Console.WriteLine("Перестить вправо");
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
