408b6ad2e29a0a231d8f1afe7fbda100c7af30ca
Pavel Litvinovich <21asdf@mail.ru>
2019-12-22 22:42:05 +0300

    hm_06.Task3

---
+++ hm_06.Task3/hm_06.Task3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace hm_06.Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Int64[] array1 = new Int64[100_000_000];             //создаем массив
+
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();                                   //запускаем таймер
+
+            for (int i = 0; i < array1.Length; i++)              //записываем в массив случайные числа
+            {
+                Int64 num = new Random().Next(0, 1000);
+                array1[i] = num;
+            }
+            stopwatch.Stop();                                    //останавливаем таймер
+            Console.WriteLine("Время выполнения моего метода: " + stopwatch.Elapsed.ToString());
+
+            stopwatch.Start();                                   //запускаем таймер
+            Array.Reverse(array1);                               //делаем реверс массива
+            stopwatch.Stop();                                    //останавливаем таймер
+            Console.WriteLine("Время выполнения Reverse:      " + stopwatch.Elapsed.ToString());
+            Console.ReadKey();
+        }
+    }
+}
