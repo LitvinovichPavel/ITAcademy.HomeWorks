a9a5cc7b165cdf3e27b92a1593a1e4d43026f618
Pavel Litvinovich <21asdf@mail.ru>
2020-01-05 23:13:37 +0300

    hm_8.Task2

---
+++ hm_8.Task2/hm_8.Task2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace hm_8.Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee moneyPerson = new Employee();
+            moneyPerson.Money();
+        }
+    }
+}
