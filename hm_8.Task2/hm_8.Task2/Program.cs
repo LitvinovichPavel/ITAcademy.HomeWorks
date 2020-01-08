b7d31c821bc518487e6e60315b22bd4274f5096c
Pavel Litvinovich <21asdf@mail.ru>
2020-01-08 21:05:49 +0300

    hm_8.Task2.protected

--- hm_8.Task2/hm_8.Task2/Program.cs
+++ hm_8.Task2/hm_8.Task2/Program.cs
@@ -1,13 +1,56 @@
 ﻿using System;
 
-namespace hm_8.Task2
+namespace AssemblyTwo
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Employee moneyPerson = new Employee();
-            moneyPerson.Money();
+            Man man = new Man();
+            man.ManMethod();
+
+            Boy boy = new Boy();
+            boy.BoyMethod();
+
+            Women women = new Women();
+            women.WomenMethod();
+
+            Girl girl = new Girl();
+            girl.GirlMethod();
+        }
+    }
+    class Women
+    {
+        protected int ageWomen = 50;
+        public void WomenMethod()
+        {
+            Console.WriteLine($"{ageWomen} years old women");
+        }
+    }
+    class Man
+    {
+        protected byte ageMan = 50;
+        public void ManMethod()
+        {
+            Console.WriteLine($"{ageMan} years old man");
+        }
+    }
+    class Girl : Women
+    {
+        public void GirlMethod()
+        {
+            int ageGirl;
+            ageGirl = ageWomen - 40;
+            Console.WriteLine($"{ageGirl} years old girl");
+        }
+    }
+    class Boy : Man
+    {
+        public void BoyMethod()
+        {
+            int ageBoy;
+            ageBoy = ageMan - 40;
+            Console.WriteLine($"{ageBoy} years old boy");
         }
     }
 }
