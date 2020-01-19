4fc377fdc87ec90ea154eff7b6e2c5776d86b073
Pavel Litvinovich <21asdf@mail.ru>
2020-01-19 18:52:00 +0300

    hm_09.airport

---
+++ hm_09.airport/hm_09.airport/Greeting.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace hm_09.airport
+{
+    class Greeting
+    {
+        private const string _nameAirport = "аэропорт имени Маши и Медведя"; // название аэропорта 
+        public void Hello() // приветствие
+        {
+            byte realTime;
+            realTime = Convert.ToByte(DateTime.Now.Hour); // приветствуем исходя из реального времени
+            string timeOfDay;
+
+            if (realTime >= 6 && realTime < 12)
+            {
+                timeOfDay = "Доброе утро! ";
+            }
+            else if (realTime >= 12 && realTime < 18)
+            {
+                timeOfDay = "Добрый день! ";
+            }
+            else if (realTime >= 18 && realTime < 21)
+            {
+                timeOfDay = "Добрый вечер! ";
+            }
+            else
+            {
+                timeOfDay = "Доброй ночи! ";
+            }
+
+            Console.WriteLine($"{timeOfDay}Вас приветствует {_nameAirport}!");
+        }
+    }
+}
---
+++ hm_09.airport/hm_09.airport/Passanger.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace hm_09.airport
+{
+    class Passanger
+    {
+        private string _surname;                                 // фамилия пассажира
+        private string _name;                                    // имя пассажира
+        private string _patronymic;                              // отчество пассажира
+
+        public void FullName()
+        {
+            Console.Write("Введите Вашу фамилию: ");
+            string nameSurname;
+            nameSurname = Console.ReadLine();
+            Passanger surname = new Passanger();
+            surname.Surname = nameSurname;
+
+            Console.Write("Введите Ваше имя: ");
+            string nameName;
+            nameName = Console.ReadLine();
+            Passanger name = new Passanger();
+            name.Name = nameName;
+
+            Console.Write("Введите Ваше отчество: ");
+            string namePatronymic;
+            namePatronymic = Console.ReadLine();
+            Passanger patronymic = new Passanger();
+            patronymic.Patronymic = namePatronymic;
+        }
+        public string Surname
+        {
+            get
+            {
+                return _surname;
+            }
+            set
+            {
+                _surname = value;
+            }
+        }
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+        public string Patronymic
+        {
+            get
+            {
+                return _patronymic;
+            }
+            set
+            {
+                _patronymic = value;
+            }
+        }
+    }
+}
