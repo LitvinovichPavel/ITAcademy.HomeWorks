c9ae94851a3732aa28c93586140fdd83795d280c
Pavel Litvinovich <21asdf@mail.ru>
2020-01-30 16:29:59 +0300

    lecture_14.Task1

---
+++ lecture_14.Task1/lecture_14.Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace lecture_14.Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   // задачу решил еще на самостоятельной, но не получилось скунуть,
+            // поэтому отправляю только сейчас (честно - честно)
+            StringClass stringClass = new StringClass();
+            stringClass.TextMethod();
+        }
+    }
+}
---
+++ lecture_14.Task1/lecture_14.Task1/StringClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lecture_14.Task1
+{
+    class StringClass
+    {
+        string text = "12345abdfiehfilhief";
+        public void TextMethod()
+        {
+            text = text.Substring(0, 5) + "...";
+            Console.WriteLine(text);
+        }
+    }
+}
