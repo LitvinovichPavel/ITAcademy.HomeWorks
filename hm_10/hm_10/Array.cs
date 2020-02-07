e679d1449542033b99c2af80e76776e62ba3dfe7
Pavel Litvinovich <21asdf@mail.ru>
2020-02-07 20:01:17 +0300

    hm_10

--- hm_10/hm_10/Array.cs
+++ hm_10/hm_10/Array.cs
@@ -4,8 +4,45 @@ using System.Text;
 
 namespace hm_10
 {
-    class Array
+    public class Kata
     {
+        public static int[] Remove(int[] integerList, int[] valuesList)
+        {
+            int count = integerList.Length;
+            for (int i = 0; i < integerList.Length; i++)
+            {
+                for (int j = 0; j < valuesList.Length; j++)
+                {
+                    if (integerList[i].Equals(valuesList[j]))
+                    {
+                        integerList[i] = 0;
+                        count--;
+                    }
+                }
+            }
 
+            int[] answer = new int[count];
+
+            count = 0;
+            for (int i = 0; i < integerList.Length; i++)
+            {
+                if (integerList[i] != 0)
+                {
+                    answer[count] = integerList[i];
+                    count++;
+                }
+            }
+
+            return answer;
+        }
+    }
+    public class ClassKata
+    {
+        public void MethodKata()
+        {
+            int[] integerList = new int[] { 1, 1, 2, 3, 1, 2, 3, 4 };
+            int[] valuesList = new int[] { 1, 3 };
+            Kata.Remove(integerList, valuesList);
+        }
     }
 }
--- hm_10/hm_10/Program.cs
+++ hm_10/hm_10/Program.cs
@@ -36,6 +36,7 @@ namespace hm_10
             //5.https://www.codewars.com/kata/563089b9b7be03472d00002b
             //Define a method/function that removes from a given array
             //of integers all the values contained in a second array.
+
         }
     }
 }
